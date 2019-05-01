8167167ef48bda6d99bd468eece79de85242ffcd
Arnie97 <arnie97@gmail.com>
2019-05-02 03:44:51 +0800

    Import modules automatically when starting iPython.

---
+++ .ipython/profile_default/startup/imports.py
@@ -0,0 +1,16 @@
+import base64
+import json
+import os
+import re
+import requests
+import sys
+import time
+from collections import *
+from datetime import datetime, date, timedelta
+from glob import *
+from functools import *
+from itertools import *
+from operator import attrgetter, itemgetter, methodcaller
+from pprint import pprint
+from pyquery import PyQuery
+from unicodedata import lookup, name, category
